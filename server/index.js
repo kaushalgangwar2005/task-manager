8e0e14bb8d5fc96d46f865e1105c2121389cf26e
Kaushal Gangwar <Kaushalgangwar833@gmail.com>
2026-05-01 12:23:48 +0530

    fix port for railway

--- server/index.js
+++ server/index.js
@@ -32,4 +32,8 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
